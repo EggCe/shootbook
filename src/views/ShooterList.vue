d5650f7cbd1fe0e69389836e6dac74369bba2bb3
Christian <ecklc-1995@web.de>
2019-12-24 20:51:47 +0100

    added express and axios

--- src/views/ShooterList.vue
+++ src/views/ShooterList.vue
@@ -8,11 +8,22 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
   name: "home",
   components: {
     // HelloWorld
+  },
+  mounted() {
+    this.getEintraege();
+  },
+  methods: {
+    getEintraege() {
+      axios.get("http://localhost:3000/eintraege").then(response => {
+        // eslint-disable-next-line no-console
+        console.log(response);
+      });
+    }
   }
 };
 </script>
--- srv/index.js
+++ srv/index.js
@@ -1,19 +1,38 @@
-import express from 'express';
+import express from "express";
+import mysql from "mysql";
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "schiessbuch"
+});
+
 // import socketIO from "socket.io";
 
 export default (app, http) => {
-  // app.use(express.json());
-  //
-  // app.get('/foo', (req, res) => {
-  //   res.json({msg: 'foo'});
-  // });
-  //
+  app.use(express.json());
+
+  app.get("/eintraege", (req, res) => {
+    let result = {};
+    connection.connect();
+
+    connection.query("SELECT * FROM eintraege", function(err, rows, fields) {
+      if (err) throw err;
+      console.log("The solution is: ", rows);
+      result = rows[0];
+      res.json({ ergebnis: rows });
+    });
+
+    connection.end();
+  });
+
   // app.post('/bar', (req, res) => {
   //   res.json(req.body);
   // });
-  // 
+  //
   // optional support for socket.io
-  // 
+  //
   // let io = socketIO(http);
   // io.on("connection", client => {
   //   client.on("message", function(data) {
@@ -21,4 +40,4 @@ export default (app, http) => {
   //   });
   //   client.emit("message", "Welcome");
   // });
-}
+};
